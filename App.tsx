0f220b9d248982f7dd3cff3ed5bc4fc8a7929c3c
Ednan <ednanjhony@yahoo.com.br>
2021-07-06 07:53:27 -0300

    done some changes

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 
-import { SignIn } from './src/screens/SignIn'
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
 import { StatusBar } from 'react-native';
 
 export default function App() {
@@ -20,13 +21,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar 
         barStyle="light-content" 
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
-    </>
+      <Home />
+    </Background>
   )
 }
