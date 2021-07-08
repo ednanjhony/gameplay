83644dc8d1f970dee56af78b0f2bf73aa459f124
Ednan <ednanjhony@yahoo.com.br>
2021-07-08 07:11:05 -0300

    done some changes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
+
 import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from './styles';
 
@@ -9,8 +12,12 @@ export function Home() {
     <View>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
       </View>
 
+      <View>
+        <CategorySelect />
+      </View>
     </View>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,12 +4,19 @@ import {
   Text, 
   Image, 
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/ButtonIcon'
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles'
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
+
   return (
     <View style={styles.container}>
      <Image 
@@ -32,7 +39,7 @@ export function SignIn() {
 
         <ButtonIcon 
           title="Entrar com discord" 
-          activeOpacity={0.7}
+          onPress={handleSignIn}
         />
       </View>
     </View>
